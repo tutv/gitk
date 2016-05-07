56834437432197251a3ca9950470790c9a9d25df
Tu TV <tutv95@gmail.com>
2016-05-08 02:18:29 +0700

    __

--- index.js
+++ index.js
@@ -11,9 +11,16 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+/**
+ * Init git puller
+ * @type {Puller}
+ */
+var gitPuller = new Puller(shell);
+gitPuller.setDir(config.dir);
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-    res.send('hello world');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.all('/catch', function (req, res) {
@@ -29,17 +36,19 @@ app.all('/catch', function (req, res) {
     }
 
     if (user_agent.indexOf('GitHub') >= 0) {//GitHub
-        var gitPuller = new Puller(shell);
-        gitPuller.setDir(config.dir);
         var pull_result = gitPuller.pull('origin master');
 
         res.json(pull_result);
     } else if (user_agent.indexOf('Bitbucket') >= 0) {//Bitbucket
+        pull_result = gitPuller.pull('origin master');
 
+        res.json(pull_result);
     } else {//Other
         res.status(404).send();
     }
 });
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(2369, function () {
     console.log('http://localhost:2369');
