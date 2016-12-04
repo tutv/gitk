e5158dea5741445db2b58caef71ee380b448ec28
Max <tutv95@gmail.com>
2016-12-05 00:15:11 +0700

    Debug

--- services/git.service.js
+++ services/git.service.js
@@ -6,10 +6,13 @@ module.exports.pull = function (dir, branch) {
     branch = branch || 'origin master';
 
     return new Promise((resolve, reject) => {
+        console.log(`cd ${dir}; git pull ${branch}`);
         shell.exec(`cd ${dir}; git pull ${branch}`, {async: true}, (code, stdout, stderr) => {
             if (code == 0) {
+                console.log(stdout);
                 resolve(stdout);
             } else {
+                console.log(stderr);
                 reject(stderr);
             }
         });
