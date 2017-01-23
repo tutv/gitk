d4bc7a0db073e5a59dbde143cd8d524af767d4d7
Max <tutv95@gmail.com>
2017-01-23 11:59:56 +0700

    Updated git service

--- services/git.service.js
+++ services/git.service.js
@@ -3,7 +3,7 @@
 const shell = require('shelljs');
 
 module.exports.pull = function (dir, branch) {
-    branch = branch || 'origin master';
+    branch = branch || '';
 
     return new Promise((resolve, reject) => {
         shell.exec(`cd ${dir}; git pull ${branch}`, {async: true}, (code, stdout, stderr) => {
